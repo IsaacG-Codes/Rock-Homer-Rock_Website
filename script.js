8c21b0cbd4b2f5c4a3ea1d9e56ff7ef9faba089e
IsaacG-Codes <Isaac.Gao@students.jmcss.org>
2023-12-07 13:04:40 -0600

    cday 4
    cday 4

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function changeColor(){
+function changeImage(){
     document.querySelector('.box').computedStyleMap.color = 'blue';
+}
+var imgs=document.getElementsByTagName('img'); 
+
+var init = 7;
+console.log(imgs[5].src);
+function changeLight(){
+var target = document.getElementById('imaget');
+
+var firstImage = imgs[0].src + "";
+if(init < 9 ){
+target.src = imgs[init].src;
+init = init +1;
+
+}
+else{
+init = 7;
+target.src = imgs[init].src;
+}
 }
