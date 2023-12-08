7d98c69b7cd6a643dfb33b6230b8f801fdcf23ca
IsaacG-Codes <Isaac.Gao@students.jmcss.org>
2023-12-08 08:34:16 -0600

    day5 p2
    day5 p2

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ init = 7;
 target.src = imgs[init].src;
 }
 }
+function resizeImage(img) {
+    img.style.width = "150px";
+    img.style.height = "150px";
+    }
+
+
+function swap(argument) {
+    var img1, img2;
+    img1=document.getElementById('img1').src;
+    img2=document.getElementById('img2').src;
+    document.getElementById('img2').src=img1
+    document.getElementById('img1').src=img2
+    resizeImage('img2')
+
+}
+
+
+
+
+
+/*
+ var img3, img4, img5, img6
+
+img3=document.getElementById('img3').src;
+    img4=document.getElementById('img4').src;
+    img5=document.getElementById('img5').src;
+    img6=document.getElementById('img6').src;
+    **/
+
+    /*var resizeImage(img) = swap {
+    
+}**/
+    
+    /*if swap(argument) {
+        
+    }**/
